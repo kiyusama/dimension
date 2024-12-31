11b0f64a5e8422e31e82773225a765bdfa24036e
kiyusama <kiyusama328@gmail.com>
2024-12-31 23:06:29 +0900

    add cube

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::char;
+
 use tokio::time::{sleep, Duration};
 
 const WIDTH: usize = 99;
@@ -5,11 +7,13 @@ const HEIGHT: usize = 31;
 
 const Z_NEAR: f32 = -30.0;
 const Z_FAR: f32 = -300.0;
-const DISTANCE_CAM: f32 = 150.0;
+const DISTANCE_CAM: f32 = 100.0;
 
-const ROTATE_SPEED_X: f32 = 0.03;
-const ROTATE_SPEED_Y: f32 = 0.02;
-const ROTATE_SPEED_Z: f32 = 0.05;
+const ROTATE_SPEED_X: f32 = 0.05;
+const ROTATE_SPEED_Y: f32 = 0.04;
+const ROTATE_SPEED_Z: f32 = 0.03;
+
+const WORD_ASPECT: f32 = 0.5;
 
 #[tokio::main]
 async fn main() {
@@ -38,7 +42,7 @@ async fn main() {
         y_rad += ROTATE_SPEED_Y;
         z_rad += ROTATE_SPEED_Z;
 
-        sleep(Duration::from_secs_f32(0.01)).await;
+        sleep(Duration::from_secs_f32(0.015)).await;
     }
 }
 
@@ -49,60 +53,80 @@ fn update_grid(
     y_rad: f32,
     z_rad: f32,
 ) {
-    let mut ch = '@';
+    render_cube(grid, z_buffer, x_rad, y_rad, z_rad);
+    //render_donut(grid, z_buffer, x_rad, y_rad, z_rad);
+}
 
-    let mut i = -30.0;
-    while i <= 30.0 {
-        let mut j = -30.0;
-        while j <= 30.0 {
-            let x = i;
-            let y = j;
-            let z = 30.0;
-
-            let x_rotated = rotate_x(x, y, z, x_rad, y_rad, z_rad);
-            let y_rotated = rotate_y(x, y, z, x_rad, y_rad, z_rad);
-            let z_rotated = rotate_z(x, y, z, x_rad, y_rad) - DISTANCE_CAM;
-
-            let x_screen = to_x_screen(x_rotated, z_rotated) as usize;
-            let y_screen = to_y_screen(y_rotated, z_rotated) as usize;
-            let depth = to_z_buffer(z_rotated);
-
-            if depth < z_buffer[y_screen][x_screen] {
-                z_buffer[y_screen][x_screen] = depth;
-                grid[y_screen][x_screen] = ch;
-            }
+fn render_cube(
+    grid: &mut [[char; WIDTH]; HEIGHT],
+    z_buffer: &mut [[f32; WIDTH]; HEIGHT],
+    x_rad: f32,
+    y_rad: f32,
+    z_rad: f32,
+) {
+    let cube_width = 30.0;
+    let mut i = -cube_width;
+    while i <= cube_width {
+        let mut j = -cube_width;
+        while j <= cube_width {
+            set_surface(grid, z_buffer, i, j, cube_width, x_rad, y_rad, z_rad, '@');
+            set_surface(grid, z_buffer, i, cube_width, j, x_rad, y_rad, z_rad, '!');
+            set_surface(grid, z_buffer, cube_width, i, j, x_rad, y_rad, z_rad, '#');
+            set_surface(grid, z_buffer, i, j, -cube_width, x_rad, y_rad, z_rad, '=');
+            set_surface(grid, z_buffer, i, -cube_width, j, x_rad, y_rad, z_rad, '$');
+            set_surface(grid, z_buffer, -cube_width, i, j, x_rad, y_rad, z_rad, '*');
 
             j += 2.0;
         }
         i += 2.0;
     }
+}
 
-    //test
-    let mut ch = ';';
-    let mut i = -30.0;
-    while i <= 30.0 {
-        let mut j = -30.0;
-        while j <= 30.0 {
-            let x = 30.0;
-            let y = j;
-            let z = i;
+fn render_donut(
+    grid: &mut [[char; WIDTH]; HEIGHT],
+    z_buffer: &mut [[f32; WIDTH]; HEIGHT],
+    x_rad: f32,
+    y_rad: f32,
+    z_rad: f32,
+) {
+    let pi = std::f32::consts::PI;
+    let radius = 40.0;
+    let circle_aspect = 1.0;
+    let ch = '@';
 
-            let x_rotated = rotate_x(x, y, z, x_rad, y_rad, z_rad);
-            let y_rotated = rotate_y(x, y, z, x_rad, y_rad, z_rad);
-            let z_rotated = rotate_z(x, y, z, x_rad, y_rad) - DISTANCE_CAM;
+    let mut i = 0.0;
+    while i <= 2.0 * pi {
+        let x = radius * i.cos() * circle_aspect;
+        let y = radius * i.sin();
+        let z = 0.0;
+        set_surface(grid, z_buffer, x, y, z, x_rad, y_rad, z_rad, ch);
 
-            let x_screen = to_x_screen(x_rotated, z_rotated) as usize;
-            let y_screen = to_y_screen(y_rotated, z_rotated) as usize;
-            let depth = to_z_buffer(z_rotated);
+        i += 0.01;
+    }
+}
 
-            if depth < z_buffer[y_screen][x_screen] {
-                z_buffer[y_screen][x_screen] = depth;
-                grid[y_screen][x_screen] = ch;
-            }
+fn set_surface(
+    grid: &mut [[char; WIDTH]; HEIGHT],
+    z_buffer: &mut [[f32; WIDTH]; HEIGHT],
+    x: f32,
+    y: f32,
+    z: f32,
+    x_rad: f32,
+    y_rad: f32,
+    z_rad: f32,
+    ch: char,
+) {
+    let x_rotated = rotate_x(x, y, z, x_rad, y_rad, z_rad);
+    let y_rotated = rotate_y(x, y, z, x_rad, y_rad, z_rad);
+    let z_rotated = rotate_z(x, y, z, x_rad, y_rad) - DISTANCE_CAM;
 
-            j += 2.0;
-        }
-        i += 2.0;
+    let x_screen = to_x_screen(x_rotated, z_rotated) as usize;
+    let y_screen = to_y_screen(y_rotated, z_rotated) as usize;
+    let depth = to_z_buffer(z_rotated);
+
+    if depth < z_buffer[y_screen][x_screen] {
+        z_buffer[y_screen][x_screen] = depth;
+        grid[y_screen][x_screen] = ch;
     }
 }
 
@@ -133,7 +157,8 @@ fn to_x_screen(x: f32, z: f32) -> f32 {
 }
 
 fn to_y_screen(y: f32, z: f32) -> f32 {
-    return (HEIGHT as f32 + (2.0 * Z_NEAR / z) * y) / 2.0;
+    // 文字のアスペクト比を鑑みて調整
+    return (HEIGHT as f32 + (2.0 * Z_NEAR / z * WORD_ASPECT) * y) / 2.0;
 }
 
 // 深度バッファを計算
